1861186d2ecf5a1484d149845922f5878b52c84f
V-Pool <vpool88@gmail.com>
2024-11-05 14:41:49 -0500

    adding my working code

--- cipher.py
+++ cipher.py
@@ -1,1 +1,18 @@
 # add your code here
+def cipher(text, shift=5):
+    encryption = ""
+    
+    for char in text:
+        if char.isalpha():  
+            offset = 65 if char.isupper() else 97
+            encryption += chr((ord(char) - offset + shift) % 26 + offset)
+        else:
+            encryption += char
+    
+    return encryption
+
+text = input("Please enter a sentence to encrypt: ")
+
+encryption = cipher(text)
+print("Your encrypted sentence is:", encryption)
+
